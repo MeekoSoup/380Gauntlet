0502f831e67a67854ccfb63371448ad831e527c2
Weston Campbell <weston.campbell1@gmail.com>
2022-04-14 11:47:51 -0700

    CameraManager, Weston_CameraTest, Art Folder
    -Just barely got started on the camera manager, not much there yet.
    -Added a test scene for myself.
    -Added an art folder where I plan to put all materials and textures. I generally like to organize artwork by who actually did it. When we get to that point I will go into more detail.

--- 380Guantlet/Assets/Scripts/Camera/CameraManager.cs
+++ 380Guantlet/Assets/Scripts/Camera/CameraManager.cs
@@ -1,9 +1,41 @@
-using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data;
 
 namespace Camera
 {
+    using UnityEngine;
+    
     public class CameraManager : MonoBehaviour
     {
-    
+        public EventNetwork eventNetwork;
+        public Camera mainCamera;
+        public List<GameObject> playerObjects = new List<GameObject>();
+
+        private void Awake()
+        {
+            playerObjects = GameObject.FindGameObjectsWithTag("Player").ToList();
+            
+            if (!mainCamera)
+                mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+            
+            if (!mainCamera)
+                Debug.Log("CameraManager: No camera with MainCamera tag found!");
+        }
+
+        private void Update()
+        {
+            Debug.Log($"CameraManager: {GetFurthestDistance()}");
+        }
+
+        private float GetFurthestDistance()
+        {
+            float dist = Vector3.Distance(mainCamera.transform.position, playerObjects[0].transform.position);
+
+            
+            
+            return dist;
+        }
     }
 }
