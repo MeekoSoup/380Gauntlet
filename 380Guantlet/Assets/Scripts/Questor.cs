7be7ea48e5467a5a7f39591034bc7bb69096b8a1
mar43ly <103211097+mar43ly@users.noreply.github.com>
2022-04-26 12:13:29 -0700

    Added roles and characters

---
+++ 380Guantlet/Assets/Scripts/Questor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questor : MonoBehaviour
+{/*Description Notes:
+  * -basically flash(speed, doudges quicker)
+  * -if pickup an extra potion, it well increase his speed
+  * and collides with any enemy without taking any damage
+  * -hair less powerful than Marlin
+  * -cannot defeat lv3 generator with a single potion
+  */
+}
---
+++ 380Guantlet/Assets/Scripts/Thor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thor : MonoBehaviour
+{/*Description Notes:
+  * -brute character(great working with others, than solo)
+  * -shot delivering 2points of 100% damage
+  * -easily kills any lv2 and lv3
+  * -disadvantage no magic and shots can't hit corners
+  * -can only pickup keys
+  */
+}
